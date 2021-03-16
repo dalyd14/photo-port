96d41a2f0c1612ce46c7b0f844d38b2e1756e3dc
dalyd14 <dalyd14@gmail.com>
2021-03-16 18:05:00 -0400

    improved contact component; navigation bar; added testing

---
+++ src/components/Contact/__tests__/index.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'
+
+import ContactForm from '..'
+
+afterEach(cleanup)
+
+describe('Contact component', () => {
+    it('renders', () => {
+        <ContactForm />
+    })
+
+    it('matches snapshot DOM node structure', () => {
+        const { asFragment } = render(<ContactForm />)
+        expect(asFragment()).toMatchSnapshot()
+    })
+
+    it('renders', () => {
+        const { getByTestId } = render(<ContactForm />)
+        expect(getByTestId('h1tag')).toHaveTextContent('Contact me')
+    })
+})
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -28,21 +28,26 @@ const ContactForm = () => {
 
     return (
         <section>
-            <h1>Contact me</h1>
+            <h1 data-testid="h1tag">Contact me</h1>
             <form id="contact-form" onSubmit={handleSubmit}>
                 <div>
                     <div>
                         <label htmlFor="name">Name:</label>
-                        <input type="text" name="name" defaultValue={name} onChange={handleChange} />
+                        <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
                     </div>
                     <div>
                         <label htmlFor="email">Email:</label>
-                        <input type="email" name="email" defaultValue={email} onChange={handleChange} />
+                        <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
                     </div>
                     <div>
                         <label htmlFor="message">Name:</label>
-                        <textarea name="message" rows="5" defaultValue={message} onChange={handleChange} />
+                        <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
                     </div>
+                    {errorMessage && (
+                        <div>
+                            <p className="error-text">{errorMessage}</p>
+                        </div>
+                    )}
                     <button type="submit">Submit</button>
                 </div>
             </form>
