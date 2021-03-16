47d5b04e6b31b4758944857610a39c5abd2ffbb3
dalyd14 <dalyd14@gmail.com>
2021-03-16 13:39:36 -0400

    handled form submit with different react events

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
-import Gallery from './components/Gallery'
+import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 
 
 function App() {
@@ -23,9 +24,8 @@ function App() {
       ></Nav>
       <main>
         <div>
-          <Gallery
-            currentCategory={currentCategory}
-          ></Gallery>
+          <ContactForm></ContactForm>
+          <Gallery currentCategory={currentCategory}></Gallery>
           <About></About>          
         </div>
       </main>
